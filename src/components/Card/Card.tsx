e9b73c2184de55d0914a4064cd9256671458a586
eddysims <eddy.s@getjobber.com>
2020-04-01 09:29:28 -0600

    first go at Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./Card.module.css";
 
 interface CardProps {
+  title: string;
   children: React.ReactNode | React.ReactNode[];
 }
 
-export const Card = ({ children }: CardProps) => {
-  return <div>Hello World - {children}</div>;
+export const Card = ({ children, title }: CardProps) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.body}>{children}</div>
+    </div>
+  );
 };
