b7e05e55c1db08f64404d8203fa105fa6930a75e
eddysims <eddy.s@getjobber.com>
2020-04-01 11:18:43 -0600

    Styles Playground

---
+++ src/gatsby-theme-docz/theme/styles.js
@@ -0,0 +1,12 @@
+import themeStyles from "gatsby-theme-docz/src/theme/styles";
+
+const styles = {
+  ...themeStyles,
+  Container: {
+    ...themeStyles.Container,
+    maxWidth: 768,
+  },
+};
+
+// eslint-disable-next-line import/no-default-export
+export default styles;
