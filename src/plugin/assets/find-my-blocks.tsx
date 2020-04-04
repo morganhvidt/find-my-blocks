ff59dd29530b89e3cea84a802afbebb0c64116b8
eddysims <eddy.s@getjobber.com>
2020-04-03 22:57:37 -0600

    Some layout on plugin page

--- src/plugin/assets/find-my-blocks.tsx
+++ src/plugin/assets/find-my-blocks.tsx
@@ -1,59 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import { useBlocks } from "../../hooks/blocks";
 
 import { Box } from "../../components/Box";
+import { Logo } from "../../components/Logo";
+import { NavigationItem } from "../../components/NavigationItem";
 import { Card } from "../../components/Card";
 import { Link } from "../../components/Link";
-import { Icon } from "../../components/Icon";
-
-import Logo from "./find-my-blocks.svg";
 
 import "./find-my-blocks.foundation.css";
 import styles from "./find-my-blocks.css";
 
 const App = () => {
+  const [activeBlock, setActiveBlock] = useState();
   const [blocks] = useBlocks();
 
   const menuItems =
     blocks.length > 0 &&
     blocks.map((block) => {
       return (
-        <li key={block.name}>
-          <Icon icon="arrow-right" /> {block.name}
-          <div>Found in {block.posts.length} posts</div>
-        </li>
+        <NavigationItem
+          key={block.name}
+          label={block.name}
+          postCount={block.posts.length}
+          active={block.name === activeBlock}
+          onClick={() => setActiveBlock(block.name)}
+        />
+      );
+    });
+
+  const activePosts =
+    activeBlock && blocks.find((block) => block.name === activeBlock).posts;
+  const postCards =
+    activePosts &&
+    activePosts.map((post) => {
+      return (
+        <Card key={post.id} title={post.title}>
+          <div>
+            Post Type: <strong>{post.postType}</strong>
+          </div>
+
+          <Link icon="edit" url={post.edit_url}>
+            Edit Post
+          </Link>
+          <Link icon="eye" url={post.post_url}>
+            View Post
+          </Link>
+
+          <p>reusable</p>
+        </Card>
       );
     });
 
   return (
     <Box className={styles.container}>
       <Box className={styles.menu}>
-        <img src={Logo} alt="React Logo" />
-        <ul>{menuItems}</ul>
+        <Box className={styles.logo}>
+          <Logo size={85} />
+        </Box>
+        <Box className={styles.nav}>{menuItems}</Box>
       </Box>
-      <Box className={styles.cards}>
-        <Card title="Example Card">
-          <p>Hello World</p>
-          <Link icon="edit" url="#">
-            Edit Post
-          </Link>
-          <Link icon="eye" url="#">
-            View Post
-          </Link>
-        </Card>
-
-        <Card title="Example Card">
-          <p>Hello World</p>
-          <Link icon="edit" url="#">
-            Edit Post
-          </Link>
-          <Link icon="eye" url="#">
-            View Post
-          </Link>
-        </Card>
-      </Box>
+      <Box className={styles.cards}>{postCards}</Box>
     </Box>
   );
 };
