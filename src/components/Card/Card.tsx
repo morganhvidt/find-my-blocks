97716857c3e6d961f2111c59b4034dd5bb2066f0
eddysims <eddy.s@getjobber.com>
2020-04-11 20:35:37 -0600

    Adds colapsable content to cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
+import { Icon } from "../Icon";
 import styles from "./Card.module.css";
 
 interface CardProps {
   title?: string;
+  toggle?: boolean;
+  initialOpen?: boolean;
   children: React.ReactNode | React.ReactNode[];
 }
 
-export const Card = ({ children, title }: CardProps) => {
+export const Card = ({
+  children,
+  title,
+  toggle = false,
+  initialOpen = true,
+}: CardProps) => {
+  const [open, setOpen] = useState<boolean>(initialOpen);
+  const bodyClass = classnames(styles.body, !open && styles.hidden);
+
   return (
     <div className={styles.card}>
-      {title && <div className={styles.title}>{title}</div>}
-      <div className={styles.body}>{children}</div>
+      {title && (
+        <CardTitle
+          title={title}
+          toggle={toggle}
+          open={open}
+          onClick={() => setOpen(!open)}
+        />
+      )}
+      <div className={bodyClass}>{children}</div>
+    </div>
+  );
+};
+
+interface CardTitleProps {
+  title: string;
+  open: boolean;
+  toggle: boolean;
+  onClick(): void;
+}
+
+const CardTitle = ({ open, toggle, title, onClick }: CardTitleProps) => {
+  return (
+    <div className={styles.title}>
+      {title}
+      {toggle && (
+        <button className={styles.button} onClick={() => onClick()}>
+          <Icon icon={open ? "chevron-up" : "chevron-down"} />
+        </button>
+      )}
     </div>
   );
 };
