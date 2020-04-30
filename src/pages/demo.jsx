afc03963da150fe5856427a93859b51a88c797f5
eddysims <eddy.s@getjobber.com>
2020-04-29 23:32:35 -0600

    Adds CardList to demo

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Layout } from "@fmb/components/Layout";
 import { Sidebar } from "@fmb/components/Sidebar";
 import { Settings } from "@fmb/components/Settings";
+import { CardList } from "@fmb/components/CardList";
 
 import data from "@fmb/data/data.json";
 
@@ -11,12 +12,22 @@ import "./demo.css";
 const Page = () => {
   const [active, setActive] = useState();
   const [navOrder, setNavOrder] = useState("a-z");
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const activeBlock = data.find((item) => item.name === active);
+    if (activeBlock != undefined && activeBlock.posts.length > 0) {
+      setCards(activeBlock.posts);
+    }
+  }, [active]);
 
   const wrapper = {
     width: "100%",
     minHeight: "100vh",
     background: "#f1f1f1",
   };
+
+  console.log(data);
 
   const container = {
     width: "100%",
@@ -40,7 +51,7 @@ const Page = () => {
             />
           }
           settings={<Settings onNavOrderChange={(val) => setNavOrder(val)} />}
-          cards={<div>Cards</div>}
+          cards={<CardList cards={cards} />}
         />
       </div>
     </div>
