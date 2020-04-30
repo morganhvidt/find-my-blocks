f8977ff1db7b49e75be8b458912e063ba25d4c5f
eddysims <eddy.s@getjobber.com>
2020-04-29 21:33:13 -0600

    Adds Test for Layout

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Layout } from "./";
 
-describe("Layout component should", () => {
-  it("render correctly", () => {
-    const tree = renderer.create(<Layout>Layout</Layout>).toJSON;
-    expect(tree).toMatchSnapshot();
-  });
+it("render correctly", () => {
+  const tree = renderer.create(<Layout />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders all areas correctly", () => {
+  const sidebar = renderer.create(<Layout sidebar="sidebar" />).toJSON();
+  const settings = renderer.create(<Layout settings="settings" />).toJSON();
+  const title = renderer.create(<Layout title="title" />).toJSON();
+  const cards = renderer.create(<Layout cards="cards" />).toJSON();
+  expect(sidebar).toMatchSnapshot();
+  expect(settings).toMatchSnapshot();
+  expect(title).toMatchSnapshot();
+  expect(cards).toMatchSnapshot();
 });
