8cd00b449ce3750afce65f568415bed3f32dccb3
eddysims <eddy.s@getjobber.com>
2020-04-29 21:35:01 -0600

    Fix the template

--- templates/component/{{ pascalCase name }}.test.tsx
+++ templates/component/{{ pascalCase name }}.test.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { {{ pascalCase name }} } from "./";
 
-describe("{{ pascalCase name }} component should", () => {
-  it("render correctly", () => {
-    const tree = renderer.create(<{{ pascalCase name }}>{{ pascalCase name }}</{{ pascalCase name }}>).toJSON;
-    expect(tree).toMatchSnapshot();
-  });
+it("render correctly", () => {
+  const tree = renderer.create(<{{ pascalCase name }}>{{ pascalCase name }}</{{ pascalCase name }}>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
