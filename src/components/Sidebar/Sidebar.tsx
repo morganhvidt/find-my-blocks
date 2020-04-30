5e13af2c99ca69e47b6feb84c5527ba6294ac982
eddysims <eddy.s@getjobber.com>
2020-04-29 21:19:09 -0600

    Adds Sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+import { Box } from "../Box";
+import { Logo } from "../Logo";
+import { InputText } from "../InputText";
+import { Filter } from "../Filter";
+import { NavigationItem } from "../NavigationItem";
+
+import styles from "./Sidebar.module.css";
+
+interface Block {
+  name: String;
+  posts: Array<String>;
+}
+
+interface SidebarProps {
+  readonly blocks: Block[];
+  readonly active?: String;
+  readonly order?: "a-z" | "z-a" | "low-high" | "high-low";
+  onClick(name: String): void;
+}
+
+export const Sidebar = ({
+  blocks,
+  active,
+  order = "a-z",
+  onClick = () => undefined,
+}: SidebarProps) => {
+  const [filter, setFilter] = useState<String>("");
+
+  switch (order) {
+    case "a-z":
+      blocks.sort((a: Block, b: Block) => (a.name > b.name ? 1 : -1));
+      break;
+    case "z-a":
+      blocks.sort((a: Block, b: Block) => (a.name < b.name ? 1 : -1));
+      break;
+    case "low-high":
+      blocks.sort((a: Block, b: Block) =>
+        a.posts.length > b.posts.length ? 1 : -1
+      );
+      break;
+    case "high-low":
+      blocks.sort((a: Block, b: Block) =>
+        a.posts.length < b.posts.length ? 1 : -1
+      );
+      break;
+    default:
+      blocks.sort((a: Block, b: Block) => (a.name > b.name ? 1 : -1));
+      break;
+  }
+
+  return (
+    <>
+      <Box className={styles.logo}>
+        <Logo size={85} />
+      </Box>
+      <InputText
+        placeholder="Filter Blocks"
+        onChange={(val) => setFilter(val)}
+      />
+      <Box className={styles.nav}>
+        {blocks && (
+          <Filter
+            data={blocks}
+            value={filter}
+            match="name"
+            renderedResults={(results) => {
+              const blocks = results.map(({ name, posts }) => (
+                <NavigationItem
+                  key={name as string}
+                  label={name}
+                  postCount={posts.length}
+                  active={name === active}
+                  onClick={() => onClick(name)}
+                />
+              ));
+
+              return blocks;
+            }}
+          />
+        )}
+      </Box>
+    </>
+  );
+};
