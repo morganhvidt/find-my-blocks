b63d82fe2c0a2d92d64f921ad2c3356e1cd47c00
eddysims <eddy.s@getjobber.com>
2020-05-02 00:24:00 -0600

    Update Link and tests

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -2,37 +2,35 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Link } from "./";
 
-describe("Link component should", () => {
-  it("render correctly", () => {
-    const tree = renderer
-      .create(<Link url="https://eddysims.com">Link</Link>)
-      .toJSON();
+it("render correctly", () => {
+  const tree = renderer
+    .create(<Link url="https://eddysims.com">Link</Link>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("can open in a new tab", () => {
+  const tree = renderer
+    .create(
+      <Link url="https://eddysims.com" openInNewTab>
+        Link
+      </Link>
+    )
+    .toJSON();
+  if (tree) {
+    expect(tree.props.target).toBe("_blank");
     expect(tree).toMatchSnapshot();
-  });
+  }
+});
 
-  it("can open in a new tab", () => {
-    const tree = renderer
-      .create(
-        <Link url="https://eddysims.com" openInNewTab>
-          Link
-        </Link>
-      )
-      .toJSON();
-    if (tree) {
-      expect(tree.props.target).toBe("_blank");
-      expect(tree).toMatchSnapshot();
-    }
-  });
+it("will render an icon", () => {
+  const tree = renderer
+    .create(
+      <Link url="https://eddysims.com" openInNewTab icon="camera">
+        Link
+      </Link>
+    )
+    .toJSON();
 
-  it("will render an icon", () => {
-    const tree = renderer
-      .create(
-        <Link url="https://eddysims.com" openInNewTab icon="camera">
-          Link
-        </Link>
-      )
-      .toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
+  expect(tree).toMatchSnapshot();
 });
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -4,8 +4,17 @@ import { Icon } from "../Icon";
 import styles from "./Link.module.css";
 
 interface LinkProps {
+  /**
+   * The URL that the link will point to.
+   */
   url: string;
+  /**
+   * An Icon that can be added before the link
+   */
   icon?: string;
+  /**
+   * Should the link open in a new tab
+   */
   openInNewTab?: boolean;
   children: React.ReactNode;
 }
