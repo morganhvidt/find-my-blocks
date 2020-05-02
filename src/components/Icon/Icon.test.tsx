fe3499adb49354eb785adbc00f1342f796acf4fd
eddysims <eddy.s@getjobber.com>
2020-05-02 00:18:57 -0600

    Update Icon Tests

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -2,21 +2,17 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Icon } from "./";
 
-describe("Icon component should", () => {
-  it("renders correctly", () => {
-    const tree = renderer.create(<Icon icon="camera" />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it("renders correctly", () => {
+  const tree = renderer.create(<Icon icon="camera" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  it("renders custom sizes", () => {
-    const tree = renderer.create(<Icon icon="camera" size={100} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it("renders custom sizes", () => {
+  const tree = renderer.create(<Icon icon="camera" size={100} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  it("renders custom colors", () => {
-    const tree = renderer
-      .create(<Icon icon="camera" color="#ff0000" />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it("renders custom colors", () => {
+  const tree = renderer.create(<Icon icon="camera" color="#ff0000" />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
