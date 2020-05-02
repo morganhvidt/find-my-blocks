9faf43aedcabb9f98ec675dd01a3d8ddb9ac15b6
eddysims <eddy.s@getjobber.com>
2020-05-01 23:59:19 -0600

    fixes filter component and tests

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,11 +1,23 @@
-interface Block {
-  name: String;
-  posts: Array<String>;
-}
+type Block = {
+  name: string;
+  posts: Array<string>;
+};
 interface FilterProps {
-  data: Array<Block>;
-  value: String;
-  match: String;
+  /**
+   * The original data that you would like to filter through
+   */
+  data: Block[];
+  /**
+   * The value or the filter query.
+   */
+  value: string;
+  /**
+   * The key that you would like to match in the data.
+   */
+  match: string;
+  /**
+   * A function that returns the filtered data as an array.
+   */
   renderedResults(results: Block[]): Block[] | undefined;
 }
 
@@ -16,7 +28,8 @@ export const Filter = ({
   renderedResults = () => undefined,
 }: FilterProps) => {
   const serachableValue = value.toLowerCase();
-  const filteredData = data.filter((item) => {
+  const filteredData: Block[] = data.filter((item) => {
+    // @ts-ignore
     const serachableItem = item[match].toLowerCase();
     return serachableItem.includes(serachableValue);
   });
