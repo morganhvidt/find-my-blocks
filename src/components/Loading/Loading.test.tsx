80f23be4f7bbd4a04d3b04e745725309b3adb5f7
eddysims <eddy.s@getjobber.com>
2020-05-02 00:28:05 -0600

    Loading docs and tests

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { Loading } from "./";
 
-describe("Loading component should", () => {
-  it("render correctly", () => {
-    const tree = renderer.create(<Loading>Loading</Loading>).toJSON;
-    expect(tree).toMatchSnapshot();
-  });
+it("render correctly", () => {
+  const tree = renderer.create(<Loading />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import styles from "./Loading.module.css";
 
-interface LoadingProps {
-  children: React.ReactNode;
-}
-
 export const Loading = () => {
   return (
     <div className={styles.loading}>
