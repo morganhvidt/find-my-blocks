c6fb04f1f58b492a91709e42b01ebfb07fc11f67
eddysims <eddy.s@getjobber.com>
2020-05-07 13:14:53 -0600

    Stop SSR of components

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,19 @@
+import { createPlugin } from "docz-core";
+
+const projectPlugin = () =>
+  createPlugin({
+    onCreateWebpackConfig: ({ stage, loaders, getConfig }) => {
+      const config = getConfig();
+
+      if (stage.includes("html")) {
+        config.module.rules.push({
+          test: /(?:components)\/.*\.(?:js|jsx|ts|tsx)$/,
+          use: loaders.null(),
+        });
+      }
+    },
+  });
+
 const defaultFont =
   "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Oxygen-Sans,Ubuntu,Cantarell,'Helvetica Neue',sans-serif";
 // eslint-disable-next-line import/no-default-export
@@ -6,6 +22,7 @@ export default {
   port: 3339,
   src: "src",
   menu: ["Find My Blocks", "Design System", "Components"],
+  plugins: [projectPlugin()],
   themeConfig: {
     fonts: {
       body: defaultFont,
