62a68262883542fdb49619f07ed6862b2d73ba84
eddysims <eddy.s@getjobber.com>
2020-05-14 12:51:11 -0600

    Bump coverage reqs

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
