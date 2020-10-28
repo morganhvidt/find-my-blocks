019dd6da1b3ce0f7b77a071b4bc9ddc377ca4f16
eddysims <eddy.s@getjobber.com>
2020-10-28 13:37:50 -0600

    Update the Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,60 +1,19 @@
-import React, { useState } from "react";
-import classnames from "classnames";
-import { Icon } from "../Icon";
-import styles from "./Card.module.css";
+import React, { PropsWithChildren } from "react";
+import { Card as WPCard, CardHeader, CardBody } from "@wordpress/components";
 
 interface CardProps {
   title?: string;
-  /**
-   * Should the card be able to open and close
-   * @default false
-   */
-  toggle?: boolean;
-  /**
-   * Should the card start open of closed
-   * @default true
-   */
-  initialOpen?: boolean;
-  children: React.ReactNode | React.ReactNode[];
 }
 
-export const Card = ({
-  children,
-  title,
-  toggle = false,
-  initialOpen = true,
-}: CardProps) => {
-  const [open, setOpen] = useState<boolean>(initialOpen);
-  const bodyClass = classnames(styles.body, !open && styles.hidden);
-
+export const Card = ({ children, title }: PropsWithChildren<CardProps>) => {
   return (
-    <div className={styles.card}>
+    <WPCard size="small">
       {title && (
-        <CardTitle
-          title={title}
-          toggle={toggle}
-          open={open}
-          onClick={() => setOpen(!open)}
-        />
+        <CardHeader isShady>
+          <strong>{title}</strong>
+        </CardHeader>
       )}
-      <div className={bodyClass}>{children}</div>
-    </div>
-  );
-};
-
-interface CardTitleProps {
-  title: string;
-  open: boolean;
-  toggle: boolean;
-  onClick(): void;
-}
-
-const CardTitle = ({ open, toggle, title, onClick }: CardTitleProps) => {
-  const titleClass = classnames(styles.title, toggle && styles.togglable);
-  return (
-    <div className={titleClass} onClick={() => toggle && onClick()}>
-      {title}
-      {toggle && <Icon icon={open ? "chevron-up" : "chevron-down"} />}
-    </div>
+      <CardBody>{children}</CardBody>
+    </WPCard>
   );
 };
