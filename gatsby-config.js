2c367b8f5f0111916d8daa4a24701583b712699e
eddysims <eddy.s@getjobber.com>
2020-10-27 22:45:20 -0600

    Remove unused gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,28 +8,11 @@ module.exports = {
         id: "UA-165299430-1",
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-alias-imports`,
-    //   options: {
-    //     alias: {
-    //       "@fmb": path.join(__dirname, "src"),
-    //     },
-    //     extensions: ["js", "ts", "tsx", "jsx"],
-    //   },
-    // },
     {
       resolve: "gatsby-plugin-root-import",
       options: {
         "@fmb": path.resolve(path.join(__dirname, "..", "src")),
       },
     },
-    // {
-    //   resolve: `gatsby-alias-imports`,
-    //   options: {
-    //     aliases: {
-    //       "@fmb": "src"
-    //     }
-    //   }
-    // }
   ],
 };
