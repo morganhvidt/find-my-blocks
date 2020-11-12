15e4180b7e89a881ba93c1d5a78a1e42bc787493
eddysims <eddy.s@getjobber.com>
2020-11-12 16:19:03 -0700

    Syltes the props table

---
+++ src/gatsby-theme-docz/components/Props/Props.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/prop-types */
+/** @jsx jsx */
+import { jsx, Box } from "theme-ui";
+import { PropsComponentProps } from "docz";
+import { Icon } from "@fmb/components/Icon";
+import * as styles from "./styles";
+
+export function Props({ props, getPropType }: PropsComponentProps) {
+  const entries = Object.entries(props).map(([key, prop]) => ({
+    key,
+    ...prop,
+  }));
+
+  return (
+    <Box sx={styles.container}>
+      {entries.map((prop) => (
+        <Prop key={prop.key} prop={prop} type={getPropType(prop)} />
+      ))}
+    </Box>
+  );
+}
+
+interface PropProps {
+  prop: PropsComponentProps;
+  type: string;
+}
+
+function Prop({ prop, type }: PropProps) {
+  if (!prop.type && !prop.flowType) return null;
+
+  const { required, name, description } = prop;
+
+  return (
+    <Box sx={styles.prop}>
+      <Box sx={styles.info}>
+        <Box sx={styles.name}>
+          {required && <Icon icon="star" size={13} />}
+          {name}
+          {getDefaultValue(prop) && (
+            <Box sx={styles.defaultText}>
+              default: <strong>{getDefaultValue(prop)}</strong>
+            </Box>
+          )}
+        </Box>
+        <Box sx={styles.type}>{type}</Box>
+      </Box>
+      {prop.description && <Box sx={styles.description}>{description}</Box>}
+    </Box>
+  );
+}
+
+function getDefaultValue({
+  defaultValue,
+  type,
+  flowType,
+}: PropsComponentProps) {
+  const propType = flowType ? flowType : type;
+
+  if (!defaultValue || !defaultValue.value) {
+    return null;
+  }
+
+  if (defaultValue.value === "''") {
+    return "[Empty string]";
+  }
+
+  if (propType && propType.name === "string") {
+    return defaultValue.value.replace(/'/g, '"');
+  }
+
+  if (typeof defaultValue.value === "object" && defaultValue.value.toString) {
+    return defaultValue.value.toString();
+  }
+
+  if (!defaultValue && !type && !flowType) {
+    return "false";
+  }
+
+  return defaultValue.value;
+}
---
+++ src/gatsby-theme-docz/components/Props/styles.ts
@@ -0,0 +1,47 @@
+export * from "gatsby-theme-docz/src/components/Props/styles";
+
+export const container = {
+  marginBottom: 3,
+};
+
+export const prop = {
+  mb: 3,
+};
+
+export const info = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  bg: "WPBackground",
+  p: 3,
+};
+
+export const name = {
+  color: "primaryDark",
+
+  svg: {
+    mr: 2,
+    mb: "-1px",
+  },
+};
+
+export const defaultText = {
+  color: "text",
+  fontSize: 1,
+  fontWeight: "body",
+};
+
+export const type = {
+  fontSize: 1,
+};
+
+export const description = {
+  border: (t) => `1px solid ${t.colors.WPBackground}`,
+  mt: "1px",
+  p: 3,
+  fontSize: 2,
+
+  "*": {
+    m: 0,
+  },
+};
