e5e4d247e3664c825d3bb01856dd6721e2bca4c8
eddysims <eddy.s@getjobber.com>
2020-12-03 15:56:59 -0700

    Remove old stuff

--- src/gatsby-theme-docz/components/Sidebar/styles.ts
+++ src/gatsby-theme-docz/components/Sidebar/styles.ts
@@ -16,27 +16,3 @@ export const navigation = {
   pt: 5,
   flex: "1",
 };
-
-// export * from "gatsby-theme-docz/src/components/Sidebar/styles";
-// import * as styles from "gatsby-theme-docz/src/components/Sidebar/styles";
-
-// export const wrapper = ({ open }) => ({
-//   ...styles.wrapper(open),
-//   p: 0,
-//   border: 0,
-//   display: "flex",
-//   flex: "1",
-//   maxWidth: 640,
-//   bg: "primary",
-// });
-
-// export const logo = {
-//   display: "block",
-//   mb: "2em",
-// };
-
-// export const container = {
-//   maxWidth: "300px",
-//   margin: "0 auto",
-//   padding: "2em 0",
-// };
