a17cfd3f8062d175af91b78a90c8e32323808754
eddysims <eddy.s@getjobber.com>
2021-10-12 16:13:31 -0600

    update from heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,17 +1,14 @@
-/** @jsx jsx */
-import { jsx, Heading as ThemeUiHeading } from "theme-ui";
+import React, { PropsWithChildren } from "react";
 
 interface HeadingProps {
   readonly level?: 1 | 2 | 3 | 4 | 5 | 6;
-  readonly children?: React.ReactNode;
 }
 
-export const Heading = ({ children, level = 1 }: HeadingProps) => {
-  const heading = `h${level}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export const Heading = ({
+  children,
+  level = 1,
+}: PropsWithChildren<HeadingProps>) => {
+  const Element = `h${level}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
-  return (
-    <ThemeUiHeading as={heading} sx={{ textTransform: "uppercase" }}>
-      {children}
-    </ThemeUiHeading>
-  );
+  return <Element style={{ textTransform: "uppercase" }}>{children}</Element>;
 };
